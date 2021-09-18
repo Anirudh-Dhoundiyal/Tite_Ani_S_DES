e81692f2f18657f7c59977d92f37a52348f69dfc
Anirudh Dhoundiyal <anirudh.dhoundiyal@jacks.sdstate.edu>
2021-09-18 17:51:07 -0500

    Good to go

--- Tite_Ani_S_DES/S_DES.h
+++ Tite_Ani_S_DES/S_DES.h
@@ -20,33 +20,35 @@
 //using namespace std
 using namespace std;
 
-
 class S_DES
 {
-private:
-	// variables
-	string cp, pt, key1, key2;
-	// functions
-	void ip(string);			// T
-	void fk(string, string);	// A
-	void e_p(string);			// A
-	void p_4(string, string);	// A
-	void s0_box(string);		// A
-	void s1_box(string);		// A
-	void sw(string);			//switch function	A
-	void ip_inverse(string);	// T
-	void p_10(string);			// T
-	void shift(string, string);	// A
-	void ls_1(string);			// T
-	void ls_2(string);			// T
-	void p_8(string,string);	// T
-	void x_or(string,string);	// T
-public:
-	S_DES();
-	~S_DES();
-	void key_gen(string);
-	void encrypt(string);
-	void decrypt(string);
+	private:
+		// variables
+		string cp,			// ciphertext
+			pt,				// plaintext 
+			key1,			// key 1 
+			key2;			// key 2
+		// functions declaration
+		void ip(string);			// T
+		void fk(string, string);	// A
+		void e_p(string);			// A
+		void p_4(string, string);	// A
+		void s0_box(string);		// A
+		void s1_box(string);		// A
+		void sw(string);			//switch function	A
+		void ip_inverse(string);	// T
+		void p_10(string);			// T
+		void shift(string, string);	// A
+		void ls_1(string);			// T
+		void ls_2(string);			// T
+		void p_8(string,string);	// T
+		void x_or(string,string);	// T
+	public:
+		S_DES();
+		~S_DES();
+		void key_gen(string);		// T	
+		void encrypt(string);		// A
+		void decrypt(string);		// A
 };
 
 #endif // !S_DES_H
